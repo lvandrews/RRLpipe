6895080cd1bdd954c50dbad518cd21cfdf8211f5
lvandrews <lva22@nau.edu>
2020-03-20 16:25:26 -0700

    initial commit

---
+++ lib/log.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+## Logging slave script
+## Author: Lela Andrews
+## Date: 2020-03-20
+## License: MIT
+## Version 0.1
+#
+
+## Define variables from inputs
+	stdout="$1"
+	stderr="$2"
+	log="$3"
+
+## Logging function
+	echo "***** stdout:" >> $log
+	if [[ ! -s $stdout ]]; then
+	echo "No output to log from stdout.
+	" >> $log
+	else
+	cat $stdout >> $log
+	echo "" >> $log
+	fi
+
+	echo "***** stderr:" >> $log
+	if [[ ! -s $stderr ]]; then
+	echo "No output to log from stderr.
+	" >> $log
+	else
+	cat $stderr >> $log
+	echo "" >> $log
+	fi
+	echo > $stdout
+	echo > $stderr
+
+exit 0
